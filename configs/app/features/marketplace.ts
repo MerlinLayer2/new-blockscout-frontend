a7c7aa4979f81ab2b7eb1e9b039c364d9ea12455
Seven <18302867820@163.com>
2024-10-22 17:17:45 +0800

    feat: fix the link

--- configs/app/features/marketplace.ts
+++ configs/app/features/marketplace.ts
@@ -5,24 +5,36 @@ import { getEnvValue, getExternalAssetFilePath } from '../utils';
 
 // config file will be downloaded at run-time and saved in the public folder
 const enabled = getEnvValue('NEXT_PUBLIC_MARKETPLACE_ENABLED');
-const configUrl = getExternalAssetFilePath('NEXT_PUBLIC_MARKETPLACE_CONFIG_URL');
+const configUrl = getExternalAssetFilePath(
+  'NEXT_PUBLIC_MARKETPLACE_CONFIG_URL',
+);
 const submitFormUrl = getEnvValue('NEXT_PUBLIC_MARKETPLACE_SUBMIT_FORM');
-const suggestIdeasFormUrl = getEnvValue('NEXT_PUBLIC_MARKETPLACE_SUGGEST_IDEAS_FORM');
-const categoriesUrl = getExternalAssetFilePath('NEXT_PUBLIC_MARKETPLACE_CATEGORIES_URL');
+const suggestIdeasFormUrl = getEnvValue(
+  'NEXT_PUBLIC_MARKETPLACE_SUGGEST_IDEAS_FORM',
+);
+const categoriesUrl = getExternalAssetFilePath(
+  'NEXT_PUBLIC_MARKETPLACE_CATEGORIES_URL',
+);
 const adminServiceApiHost = getEnvValue('NEXT_PUBLIC_ADMIN_SERVICE_API_HOST');
-const securityReportsUrl = getExternalAssetFilePath('NEXT_PUBLIC_MARKETPLACE_SECURITY_REPORTS_URL');
+const securityReportsUrl = getExternalAssetFilePath(
+  'NEXT_PUBLIC_MARKETPLACE_SECURITY_REPORTS_URL',
+);
 const featuredApp = getEnvValue('NEXT_PUBLIC_MARKETPLACE_FEATURED_APP');
-const bannerContentUrl = getExternalAssetFilePath('NEXT_PUBLIC_MARKETPLACE_BANNER_CONTENT_URL');
+const bannerContentUrl = getExternalAssetFilePath(
+  'NEXT_PUBLIC_MARKETPLACE_BANNER_CONTENT_URL',
+);
 const bannerLinkUrl = getEnvValue('NEXT_PUBLIC_MARKETPLACE_BANNER_LINK_URL');
-const ratingAirtableApiKey = getEnvValue('NEXT_PUBLIC_MARKETPLACE_RATING_AIRTABLE_API_KEY');
-const ratingAirtableBaseId = getEnvValue('NEXT_PUBLIC_MARKETPLACE_RATING_AIRTABLE_BASE_ID');
+const ratingAirtableApiKey = getEnvValue(
+  'NEXT_PUBLIC_MARKETPLACE_RATING_AIRTABLE_API_KEY',
+);
+const ratingAirtableBaseId = getEnvValue(
+  'NEXT_PUBLIC_MARKETPLACE_RATING_AIRTABLE_BASE_ID',
+);
 
 const title = 'Marketplace';
 
-const config: Feature<(
-  { configUrl: string } |
-  { api: { endpoint: string; basePath: string } }
-) & {
+const config: Feature<
+({ configUrl: string } | { api: { endpoint: string; basePath: string } }) & {
   submitFormUrl: string;
   categoriesUrl: string | undefined;
   suggestIdeasFormUrl: string | undefined;
@@ -30,25 +42,34 @@ const config: Feature<(
   featuredApp: string | undefined;
   banner: { contentUrl: string; linkUrl: string } | undefined;
   rating: { airtableApiKey: string; airtableBaseId: string } | undefined;
-}> = (() => {
+}
+> = (() => {
   if (enabled === 'true' && chain.rpcUrl && submitFormUrl) {
+    console.log('come in 1');
     const props = {
       submitFormUrl,
       categoriesUrl,
       suggestIdeasFormUrl,
       securityReportsUrl,
       featuredApp,
-      banner: bannerContentUrl && bannerLinkUrl ? {
-        contentUrl: bannerContentUrl,
-        linkUrl: bannerLinkUrl,
-      } : undefined,
-      rating: ratingAirtableApiKey && ratingAirtableBaseId ? {
-        airtableApiKey: ratingAirtableApiKey,
-        airtableBaseId: ratingAirtableBaseId,
-      } : undefined,
+      banner:
+        bannerContentUrl && bannerLinkUrl ?
+          {
+            contentUrl: bannerContentUrl,
+            linkUrl: bannerLinkUrl,
+          } :
+          undefined,
+      rating:
+        ratingAirtableApiKey && ratingAirtableBaseId ?
+          {
+            airtableApiKey: ratingAirtableApiKey,
+            airtableBaseId: ratingAirtableBaseId,
+          } :
+          undefined,
     };
 
     if (configUrl) {
+      console.log('come in 2');
       return Object.freeze({
         title,
         isEnabled: true,
@@ -56,6 +77,7 @@ const config: Feature<(
         ...props,
       });
     } else if (adminServiceApiHost) {
+      console.log('come in 3');
       return Object.freeze({
         title,
         isEnabled: true,
@@ -67,7 +89,7 @@ const config: Feature<(
       });
     }
   }
-
+  console.log('come in 4');
   return Object.freeze({
     title,
     isEnabled: false,
