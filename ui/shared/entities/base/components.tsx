d7efdf8a99361697c72bd566c1f68a9a10b96ee2
Seven <18302867820@163.com>
2024-10-31 14:51:22 +0800

    feat: fix and develop feature

--- ui/shared/entities/base/components.tsx
+++ ui/shared/entities/base/components.tsx
@@ -13,7 +13,12 @@ import LinkInternal from 'ui/shared/links/LinkInternal';
 
 import { getIconProps, type IconSize } from './utils';
 
-export type Truncation = 'constant' | 'constant_long' | 'dynamic' | 'tail' | 'none';
+export type Truncation =
+  | 'constant'
+  | 'constant_long'
+  | 'dynamic'
+  | 'tail'
+  | 'none';
 
 export interface EntityBaseProps {
   className?: string;
@@ -39,55 +44,88 @@ export interface ContainerBaseProps extends Pick<EntityBaseProps, 'className'> {
   onMouseLeave?: (event: React.MouseEvent) => void;
 }
 
-const Container = chakra(({ className, children, ...props }: ContainerBaseProps) => {
-  return (
-    <Flex
-      className={ className }
-      alignItems="center"
-      minWidth={ 0 } // for content truncation - https://css-tricks.com/flexbox-truncated-text/
-      { ...props }
-    >
-      { children }
-    </Flex>
-  );
-});
+const Container = chakra(
+  ({ className, children, ...props }: ContainerBaseProps) => {
+    return (
+      <Flex
+        className={ className }
+        alignItems="center"
+        minWidth={ 0 } // for content truncation - https://css-tricks.com/flexbox-truncated-text/
+        { ...props }
+      >
+        { children }
+      </Flex>
+    );
+  },
+);
 
-export interface LinkBaseProps extends Pick<EntityBaseProps, 'className' | 'onClick' | 'isLoading' | 'isExternal' | 'href' | 'noLink' | 'query'> {
+export interface LinkBaseProps
+  extends Pick<
+  EntityBaseProps,
+  | 'className'
+  | 'onClick'
+  | 'isLoading'
+  | 'isExternal'
+  | 'href'
+  | 'noLink'
+  | 'query'
+  > {
   children: React.ReactNode;
 }
 
-const Link = chakra(({ isLoading, children, isExternal, onClick, href, noLink }: LinkBaseProps) => {
-  const styles = {
-    display: 'inline-flex',
-    alignItems: 'center',
-    minWidth: 0, // for content truncation - https://css-tricks.com/flexbox-truncated-text/
-  };
+const Link = chakra(
+  ({
+    isLoading,
+    children,
+    isExternal,
+    onClick,
+    href,
+    noLink,
+  }: LinkBaseProps) => {
+    const styles = {
+      display: 'inline-flex',
+      alignItems: 'center',
+      minWidth: 0, // for content truncation - https://css-tricks.com/flexbox-truncated-text/
+    };
 
-  if (noLink) {
-    return <Skeleton isLoaded={ !isLoading } { ...styles }>{ children }</Skeleton>;
-  }
+    if (noLink) {
+      return (
+        <Skeleton isLoaded={ !isLoading } { ...styles }>
+          { children }
+        </Skeleton>
+      );
+    }
 
-  const Component = isExternal ? LinkExternal : LinkInternal;
+    const Component = isExternal ? LinkExternal : LinkInternal;
 
-  return (
-    <Component
-      { ...styles }
-      href={ href }
-      isLoading={ isLoading }
-      onClick={ onClick }
-    >
-      { children }
-    </Component>
-  );
-});
+    return (
+      <Component
+        { ...styles }
+        href={ href }
+        isLoading={ isLoading }
+        onClick={ onClick }
+      >
+        { children }
+      </Component>
+    );
+  },
+);
 
-export interface IconBaseProps extends Pick<EntityBaseProps, 'isLoading' | 'iconSize' | 'noIcon'> {
+export interface IconBaseProps
+  extends Pick<EntityBaseProps, 'isLoading' | 'iconSize' | 'noIcon'> {
   name: IconName;
   color?: IconProps['color'];
   borderRadius?: IconProps['borderRadius'];
 }
 
-const Icon = ({ isLoading, iconSize, noIcon, name, color, borderRadius }: IconBaseProps) => {
+const Icon = ({
+  isLoading,
+  iconSize,
+  noIcon,
+  name,
+  color,
+  borderRadius,
+}: IconBaseProps) => {
   const defaultColor = useColorModeValue('gray.500', 'gray.400');
 
   if (noIcon) {
@@ -110,62 +148,76 @@ const Icon = ({ isLoading, iconSize, noIcon, name, color, borderRadius }: IconBa
   );
 };
 
-export interface ContentBaseProps extends Pick<EntityBaseProps, 'className' | 'isLoading' | 'truncation' | 'tailLength'> {
+export interface ContentBaseProps
+  extends Pick<
+  EntityBaseProps,
+  'className' | 'isLoading' | 'truncation' | 'tailLength'
+  > {
   asProp?: As;
   text: string;
   isTooltipDisabled?: boolean;
 }
 
-const Content = chakra(({ className, isLoading, asProp, text, truncation = 'dynamic', tailLength, isTooltipDisabled }: ContentBaseProps) => {
+const Content = chakra(
+  ({
+    className,
+    isLoading,
+    asProp,
+    text,
+    truncation = 'dynamic',
+    tailLength,
+    isTooltipDisabled,
+  }: ContentBaseProps) => {
+    const children = (() => {
+      switch (truncation) {
+        case 'constant_long':
+          return (
+            <HashStringShorten
+              hash={ text }
+              as={ asProp }
+              type="long"
+              isTooltipDisabled={ isTooltipDisabled }
+            />
+          );
+        case 'constant':
+          return (
+            <HashStringShorten
+              hash={ text }
+              as={ asProp }
+              isTooltipDisabled={ isTooltipDisabled }
+            />
+          );
+        case 'dynamic':
+          return (
+            <HashStringShortenDynamic
+              hash={ text }
+              as={ asProp }
+              tailLength={ tailLength }
+              isTooltipDisabled={ isTooltipDisabled }
+            />
+          );
+        case 'tail':
+        case 'none':
+          return <chakra.span as={ asProp }>{ text }</chakra.span>;
+      }
+    })();
 
-  const children = (() => {
-    switch (truncation) {
-      case 'constant_long':
-        return (
-          <HashStringShorten
-            hash={ text }
-            as={ asProp }
-            type="long"
-            isTooltipDisabled={ isTooltipDisabled }
-          />
-        );
-      case 'constant':
-        return (
-          <HashStringShorten
-            hash={ text }
-            as={ asProp }
-            isTooltipDisabled={ isTooltipDisabled }
-          />
-        );
-      case 'dynamic':
-        return (
-          <HashStringShortenDynamic
-            hash={ text }
-            as={ asProp }
-            tailLength={ tailLength }
-            isTooltipDisabled={ isTooltipDisabled }
-          />
-        );
-      case 'tail':
-      case 'none':
-        return <chakra.span as={ asProp }>{ text }</chakra.span>;
-    }
-  })();
+    return (
+      <Skeleton
+        className={ className }
+        isLoaded={ !isLoading }
+        overflow="hidden"
+        whiteSpace="nowrap"
+        textOverflow={ truncation === 'tail' ? 'ellipsis' : undefined }
+      >
+        { children }
+      </Skeleton>
+    );
+  },
+);
 
-  return (
-    <Skeleton
-      className={ className }
-      isLoaded={ !isLoading }
-      overflow="hidden"
-      whiteSpace="nowrap"
-      textOverflow={ truncation === 'tail' ? 'ellipsis' : undefined }
-    >
-      { children }
-    </Skeleton>
-  );
-});
-
-export type CopyBaseProps = Pick<CopyToClipboardProps, 'isLoading' | 'text'> & Pick<EntityBaseProps, 'noCopy'>;
+export type CopyBaseProps = Pick<CopyToClipboardProps, 'isLoading' | 'text'> &
+Pick<EntityBaseProps, 'noCopy'>;
 
 const Copy = (props: CopyBaseProps) => {
   if (props.noCopy) {
@@ -175,10 +227,4 @@ const Copy = (props: CopyBaseProps) => {
   return <CopyToClipboard { ...props }/>;
 };
 
-export {
-  Container,
-  Link,
-  Icon,
-  Copy,
-  Content,
-};
+export { Container, Link, Icon, Copy, Content };
